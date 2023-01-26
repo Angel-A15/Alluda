dbd7038954798fa49821169412401b6798fbd8fa
Angel-A15 <aasupertwix15@gmail.com>
2023-01-26 01:41:28 -0600

    restyled homepage

--- controllers/index.js
+++ controllers/index.js
@@ -13,19 +13,4 @@ router.use((req, res) => {
 });
 
 
-module.exports = router;const router = require('express').Router();
-
-const apiRoutes = require('../controllers/api');
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
-
-router.use('/dashboard', dashboardRoutes);
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.status(404).end();
-});
-
-
 module.exports = router;
